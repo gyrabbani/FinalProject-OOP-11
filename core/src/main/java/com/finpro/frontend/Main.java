3e67f0d72e282cad234a39b536e7a6ae14a28c50
gyrabbani <rabbaniirgy@gmail.com>
2025-12-03 01:32:32 +0700

    Initial commit - Main kosong

---
+++ core/src/main/java/com/finpro/frontend/Main.java
@@ -0,0 +1,8 @@
+package com.finpro.frontend;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+
+}
