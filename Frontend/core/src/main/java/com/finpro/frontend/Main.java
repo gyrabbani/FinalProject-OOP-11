65a3ff006ad9a2bd1f1f235b982ba5cabd108979
gyrabbani <rabbaniirgy@gmail.com>
2025-12-09 23:26:11 +0700

    ngerapihin folder

--- Frontend/core/src/main/java/com/finpro/frontend/Main.java
+++ Frontend/core/src/main/java/com/finpro/frontend/Main.java
@@ -2,6 +2,7 @@ package com.finpro.frontend;
 
 import com.badlogic.gdx.ApplicationAdapter;
 
+
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
 
